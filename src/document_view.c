2f3edc8d528cc204d5aae85155b3d417303f1629
Alyssa Ross <hi@alyssa.is>
2014-05-20 22:50:55 +0100

    Better title rendering

--- src/document_view.c
+++ src/document_view.c
@@ -16,8 +16,10 @@ Layer *document_view_layer_for_bounds(struct DocumentView *self, GRect bounds) {
   Layer *layer = layer_create(bounds);
 
   GRect title_layer_frame = bounds;
-  title_layer_frame.size.h = 50;
+  title_layer_frame.size.h = 16;
   TextLayer *title_layer = text_layer_create(title_layer_frame);
+  text_layer_set_font(title_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text_alignment(title_layer, GTextAlignmentCenter);
   text_layer_set_text(title_layer, document_get_title(self->document));
   layer_add_child(layer, text_layer_get_layer(title_layer));
 
